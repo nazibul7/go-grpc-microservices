cdf99e0289d6d86d88e7d3b0f79d152ad69e2aeb
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-06-25 17:55:43 +0530

    add user role enum to auth service schema & updated refresh_token schema

--- auth-service/migrations/000001_create_users_table.up.sql
+++ auth-service/migrations/000001_create_users_table.up.sql
@@ -1,7 +1,10 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    role TEXT NOT NULL DEFAULT 'user',
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
+CREATE TYPE user_role AS ENUM ('user', 'admin');
+
+CREATE TABLE
+    users (
+        id UUID PRIMARY KEY, -- same id as user_db.users, set by UserService
+        email TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL,
+        role user_role NOT NULL DEFAULT 'user',
+        created_at TIMESTAMPTZ NOT NULL DEFAULT now ()
+    );
